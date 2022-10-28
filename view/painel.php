b89a2ac06396ab2445fc88ea710fedc258da4b07
nicollaslopes <nicollaslopesn@gmail.com>
2022-10-27 22:12:35 -0300

    ajustes nos scripts

--- view/painel.php
+++ view/painel.php
@@ -9,7 +9,6 @@
   require_once('../controller/pessoa.php');
   $dadosPessoa = Pessoa::listarPessoa();
 
-
 ?>
 
 <!DOCTYPE html>
@@ -20,11 +19,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-  <script
-      src="https://code.jquery.com/jquery-3.6.0.min.js"
-      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-      crossorigin="anonymous"
-    ></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
@@ -49,7 +44,6 @@
   <div class="container-fluid">
     
     <div class="row">
-      <!-- <div class="col">Id</div> -->
       <div class="col">Nome</div>
       <div class="col">CPF</div>
       <div class="col">RG</div>
