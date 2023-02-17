ae523f22ff0964d233ee341cc6e4789e40ad3d00
nicollaslopes <nicollaslopesn@gmail.com>
2023-02-17 14:44:37 -0300

    alterando variaveis de conf do banco de dados

--- src/model/db.php
+++ src/model/db.php
@@ -2,19 +2,19 @@
 
 class Db {
 
-    private static $host = 'localhost';
-    private static $usuario = 'root';
-    private static $senha = '';
+    private static $host = '172.18.0.2';
+    private static $usuario = 'user';
+    private static $senha = 'root';
     private static $nomeDb = 'crud-php';
 
     public static function conecta() {
 
         try {
-            $db = new PDO('mysql:host=localhost;dbname=crud-php', self::$usuario, self::$senha);
+            $db = new PDO('mysql:host=' . self::$host . ';dbname=crud-php', self::$usuario, self::$senha);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $db;
         } catch (PDOException $exception){
-            die("Erro: " . $exception->getMessage());
+            die("Error: " . $exception->getMessage());
         }
     }
 
