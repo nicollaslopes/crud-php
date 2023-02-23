9b90526ecf576694cc35f4f791a8d6f5b81adcf5
nicollaslopes <nicollaslopesn@gmail.com>
2023-02-23 16:17:20 -0300

    removendo use desnecessário

--- src/Service/UsuarioService.php
+++ src/Service/UsuarioService.php
@@ -4,7 +4,6 @@ namespace MyApp\Service;
 
 require_once '../../vendor/autoload.php';
 
-use MyApp\Model\Db;
 use MyApp\Repositories\UsuarioRepository;
 use MyApp\Service\Interface\UsuarioInterface;
 
